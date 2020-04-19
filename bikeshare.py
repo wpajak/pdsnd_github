399601a5c47f067a8014b5fd9f9fb93055258857
Wojciech Pajak <wojciechpaj@gmail.com>
2020-04-19 14:28:28 +0200

    Fix string formatting woes in time statistics

--- bikeshare.py
+++ bikeshare.py
@@ -74,7 +74,7 @@ def time_stats(df):
 
     # display the most common month
     popular_month = df['month'].mode()[0]
-    print('The most common month is: \n', popular_month)
+    print('The most common month is: {} \n'.format(popular_month))
 
     # display the most common day of week
     popular_day = df['day_of_week'].mode()[0]
